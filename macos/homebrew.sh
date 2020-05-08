babdadd9b96dc73ad6ae96774aa86ff847d82063
Víctor Moreno <victormorenotin@gmail.com>
2020-05-08 15:37:02 +0200

    Update homebrew.sh

--- macos/homebrew.sh
+++ macos/homebrew.sh
@@ -1,3 +1,4 @@
 #
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew cask install iterm2 telegram-desktop visual-studio-code google-chrome 1password adobe-connect google-drive-file-stream insomnia insomnia-designer
+brew cask install iterm2 telegram-desktop visual-studio-code google-chrome 1password adobe-connect google-drive-file-stream insomnia insomnia-designer sequel-pro fork tunnelblick unetbootin wwdc xampp zoom gcollazo-mongodb
+
