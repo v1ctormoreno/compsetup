e88a29eed45753842952e034aeaec79913e12e36
Víctor Moreno <victormorenotin@gmail.com>
2020-05-08 15:32:12 +0200

    Create homebrew.sh

---
+++ macos/homebrew.sh
@@ -0,0 +1,3 @@
+#
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew cask install iterm2 telegram-desktop visual-studio-code google-chrome 1password adobe-connect google-drive-file-stream insomnia insomnia-designer
